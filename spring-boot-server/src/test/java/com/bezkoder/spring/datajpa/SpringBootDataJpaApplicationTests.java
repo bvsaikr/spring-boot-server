5d90e85f960011f4143d146db3479ee34004dfc6
bvsaikr <88365218+bvsaikr@users.noreply.github.com>
2023-08-13 12:15:51 +0000

    Update SpringBootDataJpaApplicationTests.java

--- spring-boot-server/src/test/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplicationTests.java
+++ spring-boot-server/src/test/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplicationTests.java
@@ -1,13 +1,12 @@
 package com.bezkoder.spring.datajpa;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootDataJpaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+	
+@Test
+void contextLoads() {
+}
 }
